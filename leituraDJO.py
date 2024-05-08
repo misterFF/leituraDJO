9f2f2803bce427b235dff25b6b54a7047380c170
misterFF <32933236+misterFF@users.noreply.github.com>
2024-05-07 21:20:39 -0300

    transformação em funcional

--- leituraDJO.py
+++ leituraDJO.py
@@ -148,7 +148,67 @@ def leitura_arquivo(arquivo_leitura):
         logging.info(f"O arquivo {arquivo_leitura} foi lido com sucesso")
         return basesjuntas
 
+def deletarPlanilhaData(banco, tbl, dataTeste):
+    try:
+        banco = sqlite3.connect(banco)
+        cursor = banco.cursor()
+        cursor.execute(f"""DELETE FROM '{tbl}' WHERE
+            DATA_MOVIMENTO = '{dataTeste}'""")
+        banco.commit()
+        cursor.close()
+        banco.close()
+    except:
+        print("Não foi possível deletar os itens do banco")
 
+def verificaData(banco, tbl, dataTeste):
+    banco = sqlite3.connect(banco)
+    cursor = banco.cursor()
+    cursor.execute(f"""SELECT COUNT(CONTA_JUDICIAL) FROM '{tbl}' WHERE
+     DATA_MOVIMENTO = '{dataTeste}'""")
+    cntDados = (int(cursor.fetchall()[0][0]))
+    cursor.close()
+    banco.close()
+    if cntDados > 0:
+        return True
+    else:
+        return False
+
+def adicionarPlanilhaData(banco, tbl, base):
+    try:
+        banco = sqlite3.connect(banco)
+        cursor = banco.cursor()
+        cursor.execute(f"""create table if not exists '{tbl}' (
+                                   NUMERO_DO_PROCESSO text,
+                                   NOME_DO_TRIBUNAL text,
+                                   NOME_DA_COMARCA text,
+                                   ORGAO text,
+                                   DEPENDENCIA text,
+                                   NOME_RECLAMANTE text,
+                                   CPF_CNPJ_RECLAMANTE text,
+                                   NOME_RECLAMADO text,
+                                   CPF_CNPJ_RECLAMADO text,
+                                   CONTA_JUDICIAL text,
+                                   PARCELA text,
+                                   NUMERO_DA_GUIA text,
+                                   DATA_DO_DEPOSITO date,
+                                   VALOR_SALDO_CAPITAL float,
+                                   VALOR_CORRECAO_MONETARIA float,
+                                   VALOR_JUROS float,
+                                   VALOR_SALDO_CORRIGIDO float,
+                                   VALOR_IR float,
+                                   DATA_PROCESSAMENTO date,
+                                   CD_PRD_BNC text,
+                                   NR_SEQUENCIAL_LEGISLACAO_TRIBUTARIA text,
+                                   NUMERO_LEI_TRIBUTARIA text,
+                                   DATA_MOVIMENTO date,
+                                   ARQUIVO text,
+                                   TIPO_ARQUIVO)""")
+        base.to_sql(tbl, banco, if_exists='append', index=False)
+        banco.commit()
+        cursor.close()
+        banco.close()
+    except:
+        print("Não foi possível adicionar dados novos na planilha")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -157,154 +217,55 @@ if __name__ == '__main__':
     #caminho = "\\\\cifs-zone3\\transfarq\\SATE\\djo\\2024\\02 - Fevereiro\\Arquivos DJO\\"
     #caminho = "C:\\Users\\fsilva3\\Documents\\Arquivoslidos\\"
     caminho = "C:\\Users\\Flavio Silva\\Documents\\ArquivosConvenio\\"
-
+    banco = 'djo.sqlite3'
 
     arquivos = os.listdir(caminho)
 
     for arq in arquivos:
         caminhoTotal = caminho + arq
+        base = leitura_arquivo(caminhoTotal)
+        data = str(base.loc[1, 'DATA_MOVIMENTO'])
+        #print(data)
+        #base = leitura_arquivo(caminhoTotal)
+        if base.iloc[0]["TIPO_ARQUIVO"].count("DEPOSITOS"):
+            tbl = 'depositosacolhidos'
+            #data = str(base.at[0, 'DATA_MOVIMENTO'])
+            #print(data)
+            adicionarPlanilhaData(banco, tbl, base)
+            print(verificaData(banco, tbl, data))
+            if verificaData(banco, tbl, data) == False:
+                adicionarPlanilhaData(banco, tbl, base)
 
-        try:
-            base = leitura_arquivo(caminhoTotal)
-            if base.iloc[0]["TIPO_ARQUIVO"].count("DEPOSITOS"):
-                banco = sqlite3.connect('C:\\Users\\Flavio Silva\\Documents\\GitHub\\leituraDJOdjo.sqlite3')
-                cursor = banco.cursor()
-                cursor.execute("""create table if not exists depositosacolhidos (
-                               NUMERO_DO_PROCESSO text,
-                               NOME_DO_TRIBUNAL text,
-                               NOME_DA_COMARCA text,
-                               ORGAO text,
-                               DEPENDENCIA text,
-                               NOME_RECLAMANTE text,
-                               CPF_CNPJ_RECLAMANTE text,
-                               NOME_RECLAMADO text,
-                               CPF_CNPJ_RECLAMADO text,
-                               CONTA_JUDICIAL text,
-                               PARCELA text,
-                               NUMERO_DA_GUIA text,
-                               DATA_DO_DEPOSITO date,
-                               VALOR_SALDO_CAPITAL float,
-                               VALOR_CORRECAO_MONETARIA float,
-                               VALOR_JUROS float,
-                               VALOR_SALDO_CORRIGIDO float,
-                               VALOR_IR float,
-                               DATA_PROCESSAMENTO date,
-                               CD_PRD_BNC text,
-                               NR_SEQUENCIAL_LEGISLACAO_TRIBUTARIA text,
-                               NUMERO_LEI_TRIBUTARIA text,
-                               DATA_MOVIMENTO date,
-                               ARQUIVO text,
-                               TIPO_ARQUIVO)""")
 
-                base.to_sql('depositosacolhidos', banco, if_exists='append', index=False)
-                banco.commit()
-                cursor.close()
+        elif base.iloc[0]["TIPO_ARQUIVO"].count("FAVOR"):
+            tbl = 'regatesafavordogoverno'
+            #data = str(base.at[0, 'DATA_MOVIMENTO'])
+            #print(data)
+            adicionarPlanilhaData(banco, tbl, base)
+            print(verificaData(banco, tbl, data))
+            if verificaData(banco, tbl, data) == False:
+                adicionarPlanilhaData(banco, tbl, base)
 
-            elif base.iloc[0]["TIPO_ARQUIVO"].count("FAVOR"):
-                banco = sqlite3.connect('djo.sqlite3')
-                cursor = banco.cursor()
-                cursor.execute("""create table if not exists regatesafavordogoverno (
-                                     NUMERO_DO_PROCESSO text,
-                                     NOME_DO_TRIBUNAL text,
-                                     NOME_DA_COMARCA text,
-                                     ORGAO text,
-                                     DEPENDENCIA text,
-                                     NOME_RECLAMANTE text,
-                                    CPF_CNPJ_RECLAMANTE text,
-                                     NOME_RECLAMADO text,
-                                     CPF_CNPJ_RECLAMADO text,
-                                     CONTA_JUDICIAL text,
-                                     PARCELA text,
-                                     NUMERO_DA_GUIA text,
-                                     DATA_DO_DEPOSITO date,
-                                     VALOR_SALDO_CAPITAL float,
-                                     VALOR_CORRECAO_MONETARIA float,
-                                     VALOR_JUROS float,
-                                     VALOR_SALDO_CORRIGIDO float,
-                                     VALOR_IR float,
-                                     DATA_PROCESSAMENTO date,
-                                     CD_PRD_BNC text,
-                                     NR_SEQUENCIAL_LEGISLACAO_TRIBUTARIA text,
-                                     NUMERO_LEI_TRIBUTARIA text,
-                                     DATA_MOVIMENTO date,
-                                     ARQUIVO text,
-                                     TIPO_ARQUIVO)""")
+        elif base.iloc[0]["TIPO_ARQUIVO"].count("CONTRA"):
+            tbl = 'regatescontraogoverno'
+            #data = str(base.at[0, 'DATA_MOVIMENTO'])
+            #print(data)
+            adicionarPlanilhaData(banco, tbl, base)
+            print(verificaData(banco, tbl, data))
+            if verificaData(banco, tbl, data) == False:
+                adicionarPlanilhaData(banco, tbl, base)
 
-                base.to_sql('regatesafavordogoverno', banco, if_exists='append', index=False)
-                banco.commit()
-                cursor.close()
 
-            elif base.iloc[0]["TIPO_ARQUIVO"].count("CONTRA"):
-                banco = sqlite3.connect('djo.sqlite3')
-                cursor = banco.cursor()
-                cursor.execute("""create table if not exists regatescontraogoverno (
-                                  NUMERO_DO_PROCESSO text,
-                                  NOME_DO_TRIBUNAL text,
-                                  NOME_DA_COMARCA text,
-                                  ORGAO text,
-                                  DEPENDENCIA text,
-                                  NOME_RECLAMANTE text,
-                                 CPF_CNPJ_RECLAMANTE text,
-                                  NOME_RECLAMADO text,
-                                  CPF_CNPJ_RECLAMADO text,
-                                  CONTA_JUDICIAL text,
-                                  PARCELA text,
-                                  NUMERO_DA_GUIA text,
-                                  DATA_DO_DEPOSITO date,
-                                  VALOR_SALDO_CAPITAL float,
-                                  VALOR_CORRECAO_MONETARIA float,
-                                  VALOR_JUROS float,
-                                  VALOR_SALDO_CORRIGIDO float,
-                                  VALOR_IR float,
-                                  DATA_PROCESSAMENTO date,
-                                  CD_PRD_BNC text,
-                                  NR_SEQUENCIAL_LEGISLACAO_TRIBUTARIA text,
-                                  NUMERO_LEI_TRIBUTARIA text,
-                                  DATA_MOVIMENTO date,
-                                  ARQUIVO text,
-                                  TIPO_ARQUIVO)""")
-
-                base.to_sql('regatescontraogoverno', banco, if_exists='append', index=False)
-                banco.commit()
-                cursor.close()
-
-            elif base.iloc[0]["TIPO_ARQUIVO"].count("CONVENIO"):
-                banco = sqlite3.connect('djo.sqlite3')
-                cursor = banco.cursor()
-                cursor.execute("""create table if not exists convenioderepasses (
-                                NUMERO_DO_PROCESSO text,
-                                NOME_DO_TRIBUNAL text,
-                                NOME_DA_COMARCA text,
-                                ORGAO text,
-                                DEPENDENCIA text,
-                                NOME_RECLAMANTE text,
-                                CPF_CNPJ_RECLAMANTE text,
-                                NOME_RECLAMADO text,
-                                CPF_CNPJ_RECLAMADO text,
-                                CONTA_JUDICIAL text,
-                                PARCELA text,
-                                NUMERO_DA_GUIA text,
-                                DATA_DO_DEPOSITO date,
-                                VALOR_SALDO_CAPITAL float,
-                                VALOR_CORRECAO_MONETARIA float,
-                                VALOR_JUROS float,
-                                VALOR_SALDO_CORRIGIDO float,
-                                VALOR_IR float,
-                                DATA_PROCESSAMENTO date,
-                                CD_PRD_BNC text,
-                                NR_SEQUENCIAL_LEGISLACAO_TRIBUTARIA text,
-                                NUMERO_LEI_TRIBUTARIA text,
-                                DATA_MOVIMENTO date,
-                                ARQUIVO text,
-                               TIPO_ARQUIVO)""")
-
-                base.to_sql('convenioderepasses', banco, if_exists='append', index=False)
-                banco.commit()
-                cursor.close()
-            else:
-                print("Arquivo de resumo de movimentação")
-        except:
-            print("Não foi possivel carregar a base "+arq)
+        elif base.iloc[0]["TIPO_ARQUIVO"].count("CONVENIO"):
+            tbl = 'convenioderepasses'
+            #data = str(base.at[0, 'DATA_MOVIMENTO'])
+            #print(data)
+            adicionarPlanilhaData(banco, tbl, base)
+            print(verificaData(banco, tbl, data))
+            if verificaData(banco, tbl, data) == False:
+                adicionarPlanilhaData(banco, tbl, base)
+        else:
+            print("Arquivo de resumo de movimentação")
 
 
     #caminhoTotal = caminho + arquivo
--- testebase.py
+++ testebase.py
@@ -1,18 +1,79 @@
-import re
-import logging
 import sqlite3
 import pandas as pd
 
-base = pd.DataFrame('~\\Documents\\CONVENIO DE REPASSE DE DE 01_04_2024.xlsx')
 
-print(base.head())
-dataTeste = base.iloc[1]['DATA_MOVIMENTO']
+def deletarPlanilhaData(banco, tbl, dataTeste):
+    try:
+        banco = sqlite3.connect(banco)
+        cursor = banco.cursor()
+        cursor.execute(f"""DELETE FROM '{tbl}' WHERE
+            DATA_MOVIMENTO = '{dataTeste}'""")
+        banco.commit()
+        cursor.close()
+        banco.close()
+    except:
+        print("Não foi possível deletar os itens do banco")
 
 
-banco = sqlite3.connect('djo.sqlite3')
-cursor = banco.cursor()
-cursor.execute('SELEC COUNT(*) FROM convenioderepasse WHERE'
- 'DATA_MOVIMENTO==(%s)',dataTeste)
 
-print(cursor.fetchall())
-cursor.close()
+def verificaData(banco, tbl, dataTeste):
+    banco = sqlite3.connect(banco)
+    cursor = banco.cursor()
+    cursor.execute(f"""SELECT COUNT(CONTA_JUDICIAL) FROM '{tbl}' WHERE
+     DATA_MOVIMENTO = '{dataTeste}'""")
+    cntDados = (int(cursor.fetchall()[0][0]))
+    cursor.close()
+    banco.close()
+    if cntDados > 0:
+        return True
+    else:
+        return False
+
+
+def adicionarPlanilhaData(banco, tbl, base):
+    try:
+        banco = sqlite3.connect(banco)
+        cursor = banco.cursor()
+        cursor.execute(f"""create table if not exists '{tbl}' (
+                                   NUMERO_DO_PROCESSO text,
+                                   NOME_DO_TRIBUNAL text,
+                                   NOME_DA_COMARCA text,
+                                   ORGAO text,
+                                   DEPENDENCIA text,
+                                   NOME_RECLAMANTE text,
+                                   CPF_CNPJ_RECLAMANTE text,
+                                   NOME_RECLAMADO text,
+                                   CPF_CNPJ_RECLAMADO text,
+                                   CONTA_JUDICIAL text,
+                                   PARCELA text,
+                                   NUMERO_DA_GUIA text,
+                                   DATA_DO_DEPOSITO date,
+                                   VALOR_SALDO_CAPITAL float,
+                                   VALOR_CORRECAO_MONETARIA float,
+                                   VALOR_JUROS float,
+                                   VALOR_SALDO_CORRIGIDO float,
+                                   VALOR_IR float,
+                                   DATA_PROCESSAMENTO date,
+                                   CD_PRD_BNC text,
+                                   NR_SEQUENCIAL_LEGISLACAO_TRIBUTARIA text,
+                                   NUMERO_LEI_TRIBUTARIA text,
+                                   DATA_MOVIMENTO date,
+                                   ARQUIVO text,
+                                   TIPO_ARQUIVO)""")
+        base.to_sql(tbl, banco, if_exists='append', index=False)
+        banco.commit()
+        cursor.close()
+        banco.close()
+    except:
+        print("Não foi possível adicionar dados novos na planilha")
+
+if __name__ == '__main__':
+    base = pd.read_excel('~\\Documents\\CONVENIO DE REPASSE DE DE 01_04_2024.xlsx')
+    dataTeste = str(base.at[0, 'DATA_MOVIMENTO'])
+    banco = 'djo.sqlite3'
+    tblDados = 'convenioderepasses'
+    print(verificaData(banco, tblDados, dataTeste))
+    if verificaData(banco, tblDados, dataTeste):
+        deletarPlanilhaData(banco, tblDados, dataTeste)
+    else:
+        adicionarPlanilhaData(banco, tblDados, base)
