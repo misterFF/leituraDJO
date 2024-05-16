564ef4fa21314d6075b4b378ffc9f9e3310573f7
misterFF <32933236+misterFF@users.noreply.github.com>
2024-05-16 17:34:13 -0300

    Update interfacegráfica.py

--- interfacegráfica.py
+++ interfacegráfica.py
@@ -1,8 +1,22 @@
 import customtkinter as ct
+from tkinter import ttk
 from CTkMenuBar import *
 import sqlite3
 from datetime import datetime
 import pandas as pd
+import tkinter as tk
+
+colunas = ['NUMERO_DO_PROCESSO', 'NOME_DO_TRIBUNAL', 'NOME_DA_COMARCA',  'ORGAO',  'DEPENDENCIA',
+                                   'NOME_RECLAMANTE',  'CPF_CNPJ_RECLAMANTE',
+                                   'NOME_RECLAMADO',  'CPF_CNPJ_RECLAMADO',
+                                  'CONTA_JUDICIAL',  'PARCELA',
+                                   'NUMERO_DA_GUIA',  'DATA_DO_DEPOSITO',
+                                   'VALOR_SALDO_CAPITAL',  'VALOR_CORRECAO_MONETARIA',
+                                   'VALOR_JUROS',  'VALOR_SALDO_CORRIGIDO',
+                                   'VALOR_IR',  'DATA_PROCESSAMENTO',
+                                   'CD_PRD_BNC',  'NR_SEQUENCIAL_LEGISLACAO_TRIBUTARIA',  'NUMERO_LEI_TRIBUTARIA',
+                                   'DATA_MOVIMENTO',  'ARQUIVO', 'TIPO_ARQUIVO']
+
 
 def dataMaxima(tbl, banco = 'djo.sqlite3'):
     banco = sqlite3.connect(banco)
@@ -18,18 +32,7 @@ def retPorData(tbl, data, banco = 'djo.sqlite3'):
     dataTempo = datetime.strptime(dataTempo, '%d/%m/%Y %H:%M')
     cursor.execute(f"""SELECT * FROM '{tbl}'
                     WHERE DATA_MOVIMENTO = '{dataTempo}'""")
-    colunas = ['NUMERO_DO_PROCESSO', 'NOME_DO_TRIBUNAL', 'NOME_DA_COMARCA',  'ORGAO',  'DEPENDENCIA',
-                                   'NOME_RECLAMANTE',  'CPF_CNPJ_RECLAMANTE',
-                                   'NOME_RECLAMADO',  'CPF_CNPJ_RECLAMADO',
-                                  'CONTA_JUDICIAL',  'PARCELA',
-                                   'NUMERO_DA_GUIA',  'DATA_DO_DEPOSITO',
-                                   'VALOR_SALDO_CAPITAL',  'VALOR_CORRECAO_MONETARIA',
-                                   'VALOR_JUROS',  'VALOR_SALDO_CORRIGIDO',
-                                   'VALOR_IR',  'DATA_PROCESSAMENTO',
-                                   'CD_PRD_BNC',  'NR_SEQUENCIAL_LEGISLACAO_TRIBUTARIA',  'NUMERO_LEI_TRIBUTARIA',
-                                   'DATA_MOVIMENTO',  'ARQUIVO', 'TIPO_ARQUIVO']
     df = pd.DataFrame(cursor.fetchall(), columns=colunas)
-    print(df.head())
     return df
 
 class App(ct.CTk):
@@ -44,6 +47,8 @@ class App(ct.CTk):
         y = (hs/2)-(h/2)
         self.geometry('%dx%d+%d+%d' % (w, h, x, y))
         self.menu = self.menus()
+        self.frameTbl = self.areaTbl()
+        self.tblDados = self.tblResPesq()
 
     def menus(self):
         menu = CTkMenuBar(self)
@@ -71,6 +76,20 @@ class App(ct.CTk):
         dropdownsair.add_separator()
         dropdownsair.add_option(option="Sair", command=quit)
 
+    def areaTbl(self):
+        areatabela = ct.CTkFrame(self, width=1024, height=600)
+        areatabela.pack()
+
+    def tblResPesq(self):
+        tbl = ttk.Treeview(self.frameTbl)
+        #tbl.place(width=1024, height=600)
+        treescrolly = ttk.Scrollbar(tbl, orient="vertical", command=tbl.yview)
+        treescrollx = ttk.Scrollbar(tbl, orient="horizontal", command=tbl.xview)
+        tbl.configure(xscrollcommand=treescrollx.set, yscrollcommand=treescrolly.set)
+        treescrollx.pack(side="bottom", fill="x")
+        treescrolly.pack(side="right", fill="y")
+
+
     def rfvPorData(self):
         rfvDataWindow  = ct.CTkToplevel(self)
         rfvDataWindow.title("Resgates a Favor do Governo por Data")
@@ -87,25 +106,21 @@ class App(ct.CTk):
         dataInfo = ct.CTkEntry(rfvDataWindow, placeholder_text='dd/mm/yyyy')
         dataInfo.place(x = 150, y = 70)
         def pesquisarPorData():
-            retPorData(tbl = 'resgatesafavordogoverno', data=dataInfo.get())
+            retornoPorData = retPorData(tbl = 'resgatesafavordogoverno', data=dataInfo.get())
+            self.tblDados["column"] = list(retornoPorData.columns)
+            self.tblDados["show"] = "headings"
+            for col in self.tblDados["column"]:
+                self.tblDados.heading(col, text=col)
+            dfResultadoLinhas = retornoPorData.to_numpy().tolist()
+            for lin in dfResultadoLinhas:
+                self.tblDados.insert("", "end", values=lin)
+            self.tblDados.pack()
             fecharJanela()
         def fecharJanela():
             rfvDataWindow.destroy()
             rfvDataWindow.update()
         btnExecutar = ct.CTkButton(rfvDataWindow, text='Pesquisar', command=pesquisarPorData)
         btnExecutar.place(x = 130, y = 110)
-
-
-
-
-
-
-
-
-
-
-
-
 
 app = App()
 app.mainloop()
